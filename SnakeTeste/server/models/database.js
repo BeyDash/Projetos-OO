049ad12a7445e467a5b8ade6831f5bbcc44fd8ae
suyanne <suyannesara17@gmail.com>
2023-12-08 18:22:44 -0300

    ♻️ refact list and create update method

--- SnakeTeste/server/models/database.js
+++ SnakeTeste/server/models/database.js
@@ -12,15 +12,67 @@ export default class Database {
     fs.writeFileSync(this.filename, jsonData, 'utf-8');
   }
 
-  list() {
+  list(username) {
     // Load data from the JSON file, if it exists
     try {
       const fileData = fs.readFileSync(this.filename, 'utf-8');
       this.data = JSON.parse(fileData);
 
-      return this.data
+      if (username) {
+        // Se o username for fornecido, encontre o registro correspondente
+        const user = this.data.find((user) => user.username === username);
+
+        if(!user){
+          throw new Error('Username not found in database')
+        }
+
+        return user
+      } 
+
+      // Se nenhum username foi fornecido, retorne todos os registros
+      return this.data;
+      
     } catch (error) {
+      // 
       console.log(error);
     }
+  }
+
+  update(username, updates) {
+    // Find the user by username
+    const userToUpdate = this.list(username)
+    console.log(userToUpdate);
+    // If the user is found, update the specified keys
+    if (userToUpdate) {
+      for (const key in updates) {
+        if (Object.prototype.hasOwnProperty.call(updates, key)) {
+          userToUpdate[key] = updates[key];
+        }
+      }
+
+      // Save the updated data to the JSON file
+      this.saveToJson(this.data);
+      return true; // Indicate successful update
+    }
+
+    throw new Error('Impossible to update user, it was not found in database')
+  }
+
+
+  // TODO: Work on it latter
+  delete(username) {
+    // Find the index of the user by username
+    const userIndexToDelete = this.data.findIndex((user) => user.username === username);
+
+    // If the user is found, remove it from the array
+    if (userIndexToDelete !== -1) {
+      this.data.splice(userIndexToDelete, 1);
+
+      // Save the updated data to the JSON file
+      this.saveToJson(this.data);
+      return true; // Indicate successful deletion
+    }
+
+    return false; // Indicate user not found
   }
 }
