969383d1c0fa52c86cf35778053082989ec12633
suyanne <suyannesara17@gmail.com>
2023-12-08 18:23:44 -0300

    ✨feat: score class

--- SnakeTeste/server/index.js
+++ SnakeTeste/server/index.js
@@ -1,6 +1,7 @@
 import Register from "./models/register.js"
 import express from 'express'
 import cors from 'cors'
+import { Score } from "./models/score.js"
 
 const app = express()
 app.use(express.json())
@@ -20,6 +21,27 @@ app.post('/register', (req, res) => {
 
         // TODO: Receber esse erro no front e trata-lo redirecionanado para
         // para pag de cadastro e chamando a rota de novo
+    }
+})
+
+app.put('/update-score', (req, res) => {
+    const { username, score } = req.body
+
+    const newScore = new Score(score)
+    
+    try {
+        newScore.updateScore(username)
+        const answer = {
+            message: 'Sucessfully updated',
+            user : {
+                username,
+                score
+            }
+        }
+        return res.status(200).send(JSON.stringify(answer))
+    } catch (error) {
+        // TODO: CHECK IF IT SHOULD BE 400
+        return res.status(400).send(error.message)
     }
 })
 
